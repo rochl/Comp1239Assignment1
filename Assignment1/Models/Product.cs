a0490132afe13b87dc08ddef47ddbf17a972939f
ro_chl <romchik2k@gmail.com>
2021-02-04 17:25:42 -0500

    Product - View ,Controller and Context

---
+++ Assignment1/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {
+        
+        public int Code { get; set; }
+        [Required(ErrorMessage = "Please enter a product name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a product price.")]
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+
+        }
+    }
+}
